a61f017c1de17ca34a61bfd87ba00151f66c5095
Evgen <genitya408@gmail.com>
2025-03-29 18:36:36 +0300

    MainActivity.kt initial

--- app/src/main/java/com/example/foodapp/Activites/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/Activites/MainActivity.kt
@@ -1,21 +1,42 @@
 package com.example.foodapp.Activites
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.foodapp.R
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        
+    }
+}
+
+@Composable
+@Preview
+fun DashboardScreen(){
+    Scaffold(
+        bottomBar = {BottomAppBar()}
+    ) {paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = colorResource(R.color.blue))
+                .padding(paddingValues = paddingValues)
+        ) {  }
     }
 }
