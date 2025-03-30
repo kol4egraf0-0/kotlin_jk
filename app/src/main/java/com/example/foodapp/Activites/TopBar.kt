aa537ce251e54d0bf8ec7e3e580ccdbb74a0a1f4
Evgen <genitya408@gmail.com>
2025-03-30 11:33:06 +0300

    textL0l

--- app/src/main/java/com/example/foodapp/Activites/TopBar.kt
+++ app/src/main/java/com/example/foodapp/Activites/TopBar.kt
@@ -8,12 +8,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.foodapp.R
 
@@ -46,7 +50,32 @@ fun TopBar(){
                 padding(horizontal = horizontalPadding, vertical = verticalPadding)
             .constrainAs(profile){
                 bottom.linkTo(bluebox.bottom)
-            })
+            }
+        )
+        Text(
+            text = "Доброе утро, мужичек",
+            fontSize = 20.sp,
+            color = Color.White,
+            modifier = Modifier
+                .constrainAs(title1){
+                    top.linkTo(profile.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        )
+        Text(
+            text = "Что делашеь сегодня??",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = Color.White,
+            modifier = Modifier
+                .constrainAs(title2){
+                    top.linkTo(profile.bottom)
+                    bottom.linkTo(whitebox.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        )
     }
 }
 
