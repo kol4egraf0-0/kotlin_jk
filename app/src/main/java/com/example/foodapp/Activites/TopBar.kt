a7e6210407e9da074cee77b65ef57d5aebfd549c
Evgen <genitya408@gmail.com>
2025-03-30 11:23:57 +0300

    image mb com

--- app/src/main/java/com/example/foodapp/Activites/TopBar.kt
+++ app/src/main/java/com/example/foodapp/Activites/TopBar.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -32,7 +34,19 @@ fun TopBar(){
                 top.linkTo(parent.top)
             }
         )
-
+        Image(painter = painterResource(R.drawable.building),
+            contentDescription = null,
+            modifier = Modifier.constrainAs(building){
+                bottom.linkTo(bluebox.bottom)
+            }
+        )
+        Image(painter = painterResource(R.drawable.profile),
+            contentDescription = null,
+            modifier = Modifier.
+                padding(horizontal = horizontalPadding, vertical = verticalPadding)
+            .constrainAs(profile){
+                bottom.linkTo(bluebox.bottom)
+            })
     }
 }
 
